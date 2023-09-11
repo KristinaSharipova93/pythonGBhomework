bb35baa601e412d527d0f80a9f97d7ad20797e4f
Кристина <56476982+KristinaSharipova93@users.noreply.github.com>
2023-09-11 21:52:45 +0500

    Update task4.py
    1 задачу решила

--- task4.py
+++ task4.py
@@ -1,1 +1,7 @@
+# Напишите функцию транспонирования матрицы.
 
+matrix = [ [7, 8, 9], [9, 7, 5] ] #coздаем матрицу
+trans_matrix = [[matrix[i][j] for i in range (len(matrix))] for j in range(len(matrix[0]))]
+#использую 2 вложенных цикла для генерации наших списков
+print(matrix)
+print(trans_matrix)
